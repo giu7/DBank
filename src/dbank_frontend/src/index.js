cf6593582ebaa25b5a305cc8a29b61e1ad80a69c
Giuseppe Piano <giuseppe.piano98@gmail.com>
2022-12-24 13:46:34 +0100

    checkBalance from fe

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,19 +1,8 @@
 import { dbank_backend } from "../../declarations/dbank_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+window.addEventListener('load', async function() {
+  console.log('loading finished');
+  const currentAmount = await dbank_backend.checkBalance();
+  console.log(currentAmount);
+  this.document.getElementById('value').innerText = Math.round(currentAmount * 100) / 100; //to round to 2 decimal
+})
